11804fb31263a74433b543d860922e60af0fc579
lordm <mirodilov.777@mail.ru>
2023-10-18 16:40:21 +0500

    Finished

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("https://chat.openai.com/")
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportZoom(true)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack())
+            webView.goBack()
+        else
+            super.onBackPressed()
+    }
+}
